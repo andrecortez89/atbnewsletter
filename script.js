ec250a4eced616198a66eb825f2e4c7b2e4de697
andrecortez89 <andrecortez89@gmail.com>
2025-01-14 11:29:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Initialize Supabase Client
 const supabaseUrl = 'https://YOUR-SUPABASE-URL.supabase.co';
-const supabaseKey = 'YOUR-ANON-KEY';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkeGF0YXhzZXlqYXN5ZnFvbnFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4NjQxNzEsImV4cCI6MjA1MjQ0MDE3MX0.ONElcNyD3qvJmKF4aSS5B5VDaWd8SqTnRKkUEvQ8EJc';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 // Fetch Data from Supabase and Display in the Table
